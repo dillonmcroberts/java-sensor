0d84ab3b8766ddf5bedd0a3d840ade99bb41840f
Dillon McRoberts <dillon.mcroberts@gmail.com>
2019-07-22 09:48:38 -0400

    sensor answers.

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
 public class Sensor {
 
   private int lower;
@@ -5,19 +9,62 @@ public class Sensor {
   private int[] buffer;
 
   Sensor(int lower, int higher, int[] buffer) {
+    this.lower = lower;
+    this.higher = higher;
+    this.buffer = buffer;
 
+  }
+
+  public int[] getOutput() {
+    int[] output = Arrays
+            .stream(this.buffer)
+            .filter(num -> num <= this.higher && num >= this.lower)
+            .toArray();
+    Arrays.sort(output);
+    return output;
+  }
+
+  public String toString() {
+    return Arrays
+            .toString(this.getOutput())
+            .replaceAll("[\\[\\]\\s]","");
   }
 
   public double findMean() {
-    return 0;
+    int sum = IntStream.of(this.getOutput()).sum();
+    return sum / (float) this.getOutput().length;
   }
 
   public double findMedian() {
-    return 0;
+    int[] arr = this.getOutput();
+    int length = arr.length;
+    if (length % 2 == 0) {
+      int index1 = length / 2;
+      int index2 = index1 - 1;
+      return (arr[index1] + arr[index2]) / 2.0;
+    }
+    return arr[length / 2];
   }
 
   public int findMode() {
-    return 0;
+
+    HashMap<Integer, Integer> numOccurences = new HashMap<>();
+
+    int[] arr = this.getOutput();
+    int mostCommon = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (numOccurences.containsKey(arr[i])) {
+        int count = numOccurences.get(arr[i]);
+        numOccurences.put(arr[i], count + 1);
+
+        if (count > mostCommon) {
+          mostCommon = arr[i];
+        }
+      } else
+        numOccurences.put(arr[i], 1);
+    }
+    return mostCommon;
   }
 
 
